28b63b8c01a64b6b7e9523c7db54a6ca8680ac29
Phil Wilkins <mp3monster@users.noreply.github.com>
2023-11-07 15:12:38 +0000

    Create key-protect.bat
    implementation of the key-protect batch file

---
+++ key-protect.bat
@@ -0,0 +1,22 @@
+::# Set Key File Variable:
+    Set Key="%1"
+
+::# Remove Inheritance:
+    Icacls %Key% /c /t /Inheritance:d
+
+::# Set Ownership to Owner:
+    :: # Key's within %UserProfile%:
+         Icacls %Key% /c /t /Grant %UserName%:F
+
+    :: # Key's outside of %UserProfile%:
+         TakeOwn /F %Key%
+         Icacls %Key% /c /t /Grant:r %UserName%:F
+
+::# Remove All Users, except for Owner:
+    Icacls %Key% /c /t /Remove:g "Authenticated Users" BUILTIN\Administrators BUILTIN Everyone System Users
+
+::# Verify:
+    Icacls %Key%
+
+::# Remove Variable:
+    set "Key="
